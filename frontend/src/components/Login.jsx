dbf61402d393a06372b9d8eef278ad28cfa0c85d
himanshuhestabit <himanshup.hestabit@gmail.com>
2025-03-13 15:23:09 +0530

    improved the ui part of login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,14 +5,16 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { loginSuccess } from "../redux/features/authSlice";
+
 const Login = () => {
+  const imageUrl =
+    "https://img.freepik.com/free-vector/secure-login-concept-illustration_114360-4685.jpg?t=st=1741859293~exp=1741862893~hmac=d111e44bc18cd67482ca875a45b9c9bda51cbde3576e2bcb2f3a52e4be8278b8&w=740";
   const API_URL = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors, isSubmitting },
   } = useForm();
 
@@ -36,41 +38,59 @@ const Login = () => {
 
   return (
     <div className="w-full h-screen flex items-center justify-center bg-[#1A1A1D] text-white flex-col">
-      <div className="w-full">
-        <div className="text-3xl font-bold container mx-auto mb-6 text-center">
-          <p>Login User</p>
+      <div className="bg-[#3B1C32] w-2/4 h-2/4 rounded-xl flex flex-col items-center justify-center">
+        <div>
+          <p className="text-3xl font-bold container mx-auto mb-6">
+            Login User
+          </p>
         </div>
-        <div className="w-full">
+        <div className="w-[90%] flex">
           <form
-            action=""
             onSubmit={handleSubmit(onSubmit)}
-            className="w-full container mx-auto flex flex-col gap-4"
+            className="w-full flex flex-col gap-4"
           >
             <div className="flex flex-col gap-2 w-full">
-              <label htmlFor="">Email</label>
+              <label htmlFor="email">Email</label>
               <input
-                {...register("email")}
-                className="bg-[#3B1C32] w-3/4 p-3 rounded-md"
+                {...register("email", { required: "Email is required" })}
+                className="bg-[#1A1A1D] w-3/4 p-3 rounded-md"
               />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
             </div>
             <div className="flex flex-col gap-2 w-full">
-              <label htmlFor="">Password</label>
+              <label htmlFor="password">Password</label>
               <input
-                {...register("password")}
-                className="bg-[#3B1C32] w-3/4 p-3 rounded-md"
+                type="password"
+                {...register("password", { required: "Password is required" })}
+                className="bg-[#1A1A1D] w-3/4 p-3 rounded-md"
               />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
             </div>
             <div>
               <input
-                className="bg-[#6A1E55] text-white px-4 py-2 rounded"
+                className="bg-[#1A1A1D] text-white px-4 py-2 rounded-lg hover:bg-[#101012] cursor-pointer transition-all duration-300"
                 type="submit"
                 value={isSubmitting ? "Submitting..." : "Submit"}
                 disabled={isSubmitting}
               />
             </div>
           </form>
-          <div className="text-center text-blue-300">
-            <Link to="/">Register? New user</Link>
+          <div className="text-center flex flex-col items-center justify-center">
+            <img
+              className="bg-transparent bg-cover w-[85%] h-auto pb-3"
+              src={imageUrl}
+              alt="login Image"
+            />
+            <Link
+              to="/"
+              className="cursor-pointer text-blue-200 hover:text-blue-300 transition-all duration-300"
+            >
+              New User? Register
+            </Link>
           </div>
         </div>
       </div>
